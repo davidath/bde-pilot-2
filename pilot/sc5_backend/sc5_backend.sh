c8ef83b686748313114f5bdbff6535c8b0a86e31
Thanasis Davvetas <davve.ath@gmail.com>
2017-09-12 15:19:00 +0300

    ref

---
+++ pilot/sc5_backend/sc5_backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /bde-pilot-2/pilot/detection_listener/sc5_backend/
+rm -f *.zip
+killall -9 redis-server
+killall -9 celery
+killall -9 gunicorn
+./run-redis.sh &
+celery worker -A listener.celery -f celery_log.txt --loglevel=info &
+gunicorn -b 0.0.0.0:5000 -w 2 -t 500 listener:app --log-level debug --log-file gunicorn_log.txt &
